a77f30159b4862296e061a53d0609edba151d76f
ellaisafaith <apidol.ellaisafaith17@gmail.com>
2025-03-05 23:13:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let myCoursesHtml = `
+//     <h3>My Courses:</h3>
+//     <ul>
+//         <li>IT112 - Computer Programming 1 (3.0 units)</li>
+//         <li>IT111 - Introduction to Computing (3.0 units)</li>
+//         <li>PurCom - Purposive Communication (3.0 units)</li>
+//         <li>...</li>
+//     </ul>
+// `;
+// document.querySelector(".education").innerHTML += myCoursesHtml;
